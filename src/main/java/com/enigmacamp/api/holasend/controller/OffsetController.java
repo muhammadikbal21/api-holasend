88c2e9cc03d09db3bb8feb9b9f527b8d6c94549c
Hariansyah <hariansyahhari2@gmail.com>
2021-03-24 23:47:58 +0700

    Admin Validation

--- src/main/java/com/enigmacamp/api/holasend/controller/OffsetController.java
+++ src/main/java/com/enigmacamp/api/holasend/controller/OffsetController.java
@@ -1,13 +1,30 @@
 package com.enigmacamp.api.holasend.controller;
 
+import com.enigmacamp.api.holasend.configs.jwt.JwtToken;
 import com.enigmacamp.api.holasend.models.ResponseMessage;
+import com.enigmacamp.api.holasend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.enigmacamp.api.holasend.controller.validations.RoleValidation.validateRoleAdmin;
 
 @RestController
 @RequestMapping("/offset")
 public class OffsetController {
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtToken jwtTokenUtil;
+
     private Double offset = 100.0;
+
+    private void validateAdmin(HttpServletRequest request) {
+        validateRoleAdmin(request, jwtTokenUtil, userService);
+    }
 
     @GetMapping
     public ResponseMessage<Double> getOffset() {
@@ -16,8 +33,10 @@ public class OffsetController {
 
     @PutMapping("/{input}")
     public ResponseMessage<Double> editOffset(
+            HttpServletRequest request,
             @PathVariable Double input
     ) {
+        validateAdmin(request);
         offset = input;
         return ResponseMessage.success(offset);
     }
